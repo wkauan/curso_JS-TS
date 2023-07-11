45dd5871b41fb5e741c39c6ac89e24c9a38e94c1
wkauan <kauanmbofim5@gmail.com>
2023-07-11 20:22:53 -0300

    Aula 06 adicionada

--- Exercicios/Ex01.js
+++
@@ -1,5 +0,0 @@
-/* Faça um console.log com o texto "Meu nome é "SEU NOME". Estou aprendendo JavaScript às 10 da manhã.", 
-com seu nome estando etre aspas e o data sendo do tipo number */
-
-
-console.log('Meu nome é "Kauan". Estou aprendendo JavaScript às', 10, 'da manhã.');
