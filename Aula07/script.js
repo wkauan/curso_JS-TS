e0ab492ed1d2c3677573b0d4e25adb4d9210c1f3
wkauan <kauanmbofim5@gmail.com>
2023-08-06 08:46:34 -0300

    Utilizando arrays/object/functions de forma prática

---
+++ Aula07/script.js
@@ -0,0 +1,52 @@
+// Arrays
+// const alunos = ['Luiz', 'Maria', 'João'];
+
+// alunos.push ('Eduardo'); // Adiciona um dado no final da array
+// alunos.unshift('Fabiana'); // Adiciona um dado no inicio do array
+// console.log(alunos);
+
+// alunos.pop(); // Remove um dado no final da array
+// alunos.shift(); // Remove um dado no inicio do array
+// console.log(alunos); 
+
+
+// Funções
+// function soma(x, y) {
+//     const resultado = x + y;
+//     return resultado;
+// }
+// console.log("Função normal:", soma(2, 2));
+
+// const raiz = function (n) {
+//     return n ** 0,5;
+// };
+// console.log("Função com const:", raiz(9));
+
+// const arrowFunction = af => af + 2;
+// console.log("Função com arrow function:", arrowFunction(2));
+
+
+// Objetos
+// function criaPessoa (nome, sobrenome, idade) {
+//     return {nome,sobrenome, idade};
+// }
+
+// const pessoa = criaPessoa('Kauan', 'Bomfim', 19);
+// console.log(pessoa);
+
+// const pessoa1 = {
+//     nome:'Kauan',
+//     sobrenome: 'Bomfim',
+//     idade: 19,
+
+//     fala() {
+//         console.log(`A idade de ${this.nome} ${this.sobrenome} é ${this.idade}`);
+//     },
+
+//     incrementaIdade() {
+//         this.idade++;
+//     }
+// };
+
+// pessoa1.incrementaIdade();
+// pessoa1.fala();
