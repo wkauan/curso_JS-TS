f675b74b1f4c5ac6a87e2b141aafde794da4d0d3
wkauan <kauanmbomfim5@gmail.com>
2023-10-20 09:59:32 -0300

    :sparkles: feat: Estrutura switch/case

---
+++ Aula12/main.js
@@ -0,0 +1,43 @@
+function getWeekText(diaSemana) {
+    let diaSemanaTexto;
+
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = 'Domingo';
+            return diaSemanaTexto;
+
+        case 1:
+            diaSemanaTexto = 'Segunda';
+            return diaSemanaTexto;
+
+        case 2:
+            diaSemanaTexto = 'Terça';
+            return diaSemanaTexto;
+
+        case 3:
+            diaSemanaTexto = 'Quarta';
+            return diaSemanaTexto;
+
+        case 4:
+            diaSemanaTexto = 'Quinta';
+            return diaSemanaTexto;
+
+        case 5:
+            diaSemanaTexto = 'Sexta';
+            return diaSemanaTexto;
+
+        case 6:
+            diaSemanaTexto = 'Sábado';
+            return diaSemanaTexto;
+
+        default:
+            diaSemanaTexto = '';
+            return diaSemanaTexto;
+    }
+}
+
+const data = new Date('1987-04-21 00:00:00');
+const diaSemana = data.getDay();
+const diaSemanaTexto = getWeekText(diaSemana);
+
+console.log(diaSemanaTexto);
