33380e143a4763424451cb270d6cc3ce4eeca911
wkauan <kauanmbomfim5@gmail.com>
2023-10-26 14:43:08 -0300

    :sparkles: feat: Diferenças entre var e let

--- Aula13/main.js
+++ Aula13/main.js
@@ -1,4 +1,5 @@
-let nome = 'Kauan';
+let nome = 'Rodrigo';
+let verdadeiro = true;
 
 console.log('1:', nome);
 
@@ -8,3 +9,19 @@ console.log('1:', nome);
 }
 
 console.log('3:', nome);
+
+function escopo() {
+    if(verdadeiro) {
+        let nome = 'Kauan';
+        var sobrenome = 'Bomfim';
+        console.log(nome);
+    }
+
+    console.log(sobrenome);
+}
+
+escopo();
+
+console.log('Nome 2:', nome2);
+
+var nome2 = 'Pedro';
